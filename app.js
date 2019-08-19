d9b89cd5308e689bd9ee601b12e77a27504193f7
Dan Forder <dan.forder@Nology.local>
2019-08-19 16:43:48 +0100

    functions working, tests passing

--- app.js
+++ app.js
@@ -1,14 +1,96 @@
-import morseBoard from "./data";
+const morseBoard = {
+  "0": "-----",
+  "1": ".----",
+  "2": "..---",
+  "3": "...--",
+  "4": "....-",
+  "5": ".....",
+  "6": "-....",
+  "7": "--...",
+  "8": "---..",
+  "9": "----.",
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+  ".": ".-.-.-",
+  ",": "--..--",
+  "?": "..--..",
+  "!": "-.-.--",
+  "-": "-....-",
+  "/": "-..-.",
+  "@": ".--.-.",
+  "(": "-.--.",
+  ")": "-.--.-",
+  " ": " "
+};
 
-console.log(morseBoard);
+// const $ = require("jquery");
 
-function morseToEnglish(string) {
-  return null;
+// console.log(morseBoard);
+
+function getKeyByValue(object, value) {
+  return Object.keys(object).find(key => object[key] === value);
 }
 
-function englishToMorse(string) {
-  return null;
-}
+const morseToEnglish = string => {
+  let returnString = "";
+  const morseArray = string.split(" ");
+
+  for (let index = 0; index < morseArray.length; index++) {
+    if (getKeyByValue(morseBoard, morseArray[index]) !== undefined) {
+      returnString += getKeyByValue(morseBoard, morseArray[index]);
+    } else if (morseArray[index] === "") {
+      if (morseArray[index + 1] === "") {
+        returnString += " ";
+      }
+    } else {
+      return "Invalid morse code entered";
+    }
+  }
+  return returnString;
+};
+
+const englishToMorse = string => {
+  let returnString = "";
+  for (let index = 0; index < string.length; index++) {
+    // console.log(morseBoard[string[index]]);
+    if (morseBoard[string[index]] === undefined) {
+      return "Invalid string entered";
+    }
+    returnString += morseBoard[string[index]] + " ";
+  }
+
+  return returnString.substring(0, returnString.length - 1);
+};
+
+const englishInput = document.getElementById("english-input");
+const morseInput = document.getElementById("morse-input");
+
+// console.log(englishToMorse("hello there"));
+morseToEnglish("hello");
 
 module.exports = { morseToEnglish, englishToMorse };
 
--- app.test.js
+++ app.test.js
@@ -1,30 +1,29 @@
-//Hello There
-// .... . .-.. .-.. ---     - .... . .-. .
-//General Kenobi
-// --. . -. . .-. .- .-..     -.- . -. --- -... ..
-
 const app = require("./app");
 
 test("Converts Morse Code to an English string", () => {
-  expect(app.morseToEnglish(".... . .-.. .-.. ---  - .... . .-. .")).toBe(
+  expect(app.morseToEnglish(".... . .-.. .-.. ---   - .... . .-. .")).toBe(
     "hello there"
   );
   expect(
-    app.morseToEnglish("--. . -. . .-. .- .-..  -.- . -. --- -... ..")
+    app.morseToEnglish("--. . -. . .-. .- .-..   -.- . -. --- -... ..")
   ).toBe("general kenobi");
   expect(
-    app.morseToEnglish(".---- ..--- ...--  ... .. -..- - -.-- - .-- --- -·-·--")
+    app.morseToEnglish(
+      ".---- ..--- ...--   ... .. -..- - -.-- - .-- --- -.-.--"
+    )
   ).toBe("123 sixtytwo!");
+  expect(app.morseToEnglish("hello")).toBe("Invalid morse code entered");
 });
 
 test("Converts an English string to Morse Code", () => {
   expect(app.englishToMorse("hello there")).toBe(
-    ".... . .-.. .-.. ---  - .... . .-. ."
+    ".... . .-.. .-.. ---   - .... . .-. ."
   );
   expect(app.englishToMorse("general kenobi")).toBe(
-    "--. . -. . .-. .- .-..  -.- . -. --- -... .."
+    "--. . -. . .-. .- .-..   -.- . -. --- -... .."
   );
   expect(app.englishToMorse("123 sixtytwo!")).toBe(
-    ".---- ..--- ...--  ... .. -..- - -.-- - .-- --- -·-·--"
+    ".---- ..--- ...--   ... .. -..- - -.-- - .-- --- -.-.--"
   );
+  expect(app.englishToMorse("++ ## hello")).toBe("Invalid string entered");
 });
